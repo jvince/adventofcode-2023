3268d555033bc08012bd41b1d705917dcb34a1d7
Vince Juric <vince.nightshift@gmail.com>
2023-12-01 19:48:25 +0100

    Initial commit.
    * Day 1 part 1 and 2.

---
+++ src/day-1/index.js
@@ -0,0 +1,76 @@
+const { promisify } = require('util');
+
+const readFile = promisify(require('fs').readFile);
+
+const NumberMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six:6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+};
+
+function getNumberFromMap(str) {
+  return NumberMap[str] ?? str;
+}
+
+/** 
+ * @callback StringProcessor
+ * @param {string} str
+ * @returns {number}
+ */
+
+/** @type {StringProcessor} */
+function processLine1(str) {
+  const first = str.match(/\d/);
+
+  if (!first) {
+    throw new Error(`Invalid line: ${str}. No number found.`);
+  };
+
+  const last = str.match(/\d(?=\D*$)/);
+  const n = Number.parseInt(`${first}${last}`, 10)
+
+  console.log(`got: ${n}`);
+
+  return n;
+}
+
+/** @type {StringProcessor} */
+function processLine2(str) {
+  const result = str.match(/(\d|one|two|three|four|five|six|seven|eight|nine)/g);
+  
+  if (!result) {
+    throw new Error(`Invalid line: ${str}. No number found.`);
+  };
+
+  const n = Number.parseInt(`${getNumberFromMap(result[0])}${getNumberFromMap(result[result.length - 1])}`, 10)
+
+  console.log(`got: ${n}`);
+
+  return n;
+}
+
+
+async function main() {
+  try {
+    const input = await readFile('./input', 'utf8');
+    const lines = input.split(/\r?\n/);
+
+    const result = lines.reduce((acc, line) => {
+      console.log(`processing line: ${line}`);
+      return acc + processLine2(line);
+    }, 0)
+
+    console.log(`Result: ${result}`);
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+
+main();
